8b34b4dd55d665615d3399ac31e49476dd3953b0
santimat <santimaturo1@gmail.com>
2026-04-13 19:43:05 -0300

    refactor: update astro.config.mjs to include path aliases in vite configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 import { defineConfig } from "astro/config";
 import talwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
-
+import path from "path";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
   vite: {
     plugins: [talwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+        "@icons": path.resolve("./src/icons"),
+        "@components": path.resolve("./src/components"),
+        "@assets": path.resolve("./src/assets"),
+        "@styles": path.resolve("./src/styles"),
+      },
+    },
   },
 });
