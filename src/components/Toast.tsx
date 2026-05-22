bdbe2cb475ef1491433de50280014b78cc2b8751
santimat <santimaturo1@gmail.com>
2026-05-22 15:47:43 -0300

    add classes for toast type

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -4,7 +4,7 @@ export function Toast({ children }) {
   const [toast, setToast] = useState<string | null>(null);
   const [toastType, setToastType] = useState<"success" | "error" | null>(null);
 
-  const toastClasses = ``;
+  const toastClasses = toastType === "error" ? "bg-accent/50" : "bg-darker/50";
 
-  return <span>{toast}</span>;
+  return <span className={`absolute top-4 left-4 ${toastClasses}`}>{toast}</span>;
 }
