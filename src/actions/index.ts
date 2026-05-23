93f7b40d3e430d6e211cd5793a3f6420f8d49a4f
santimat <santimaturo1@gmail.com>
2026-05-22 23:38:30 -0300

    feat: implement resend to send query emails refactor: fix form width and input paddings

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,8 +2,6 @@ import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export const server = {
   sendContactInfo: defineAction({
     accept: "json",
@@ -12,11 +10,37 @@ export const server = {
       query: z.string().min(1),
     }),
     handler: async ({ email, query }) => {
-      console.log(`Received contact form submission from ${email}: ${query}`);
+      const RESEND_API_KEY = import.meta.env.RESEND_API_KEY;
+
+      const resend = new Resend(RESEND_API_KEY);
+      const { error } = await resend.batch.send([
+        {
+          from: "n@nagarestudio.site",
+          to: "nagarestudiojs@gmail.com",
+          subject: "New Contact Query",
+          html: `
+          <p>You have received a new contact query from ${email}:</p>
+          <p>${query}</p>
+        `,
+        },
+        {
+          from: "n@nagarestudio.site",
+          to: email,
+          subject: "Thank you for contacting us!",
+          html: `
+          <p>Dear ${email},</p>
+          <p>Thank you for reaching out to us! We have received your query and will get back to you shortly.</p>
+          <p>Best regards,<br/>The Nagare Team</p>
+        `,
+        },
+      ]);
+
+      if (error) {
+        return console.error(error);
+      }
 
       return {
         success: true,
-        query: "Thank you for contacting us! We will get back to you shortly.",
       };
     },
   }),
