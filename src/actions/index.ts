c8777f4d7e1535f8b8217e9dd7c98b1c64120fc7
santimat <santimaturo1@gmail.com>
2026-05-31 14:12:10 -0300

    fix: typos and undefined elements

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,5 @@
 import { defineAction } from "astro:actions";
-import { z } from "astro:schema";
+import { z } from "astro/zod";
 import { checkRateLimit } from "@/lib/upstashLimit";
 import { sendEmail } from "@/services/sendEmail";
 
@@ -7,12 +7,13 @@ export const server = {
   sendContactInfo: defineAction({
     accept: "form",
     input: z.object({
-      email: z.string().email("el email no es válido"),
+      email: z.email("el email no es válido"),
       query: z
         .string({ error: "la consulta no puede estar vacía" })
         .min(1, { error: "la consulta no puede estar vacía" }),
     }),
     handler: async (formData, context) => {
+      const { email } = formData;
       const identifier = context?.clientAddress || email;
       await checkRateLimit(identifier);
       await sendEmail(formData);
